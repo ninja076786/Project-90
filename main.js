52aef2316be778ddc824f8537e415ee11fda5848
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-05-30 15:29:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
+function MUAHAHA(){
 var oo1=document.getElementById("player1_name_input_1").value;
 var oo2=document.getElementById("player2_name_input_2").value;
-document.getElementById("name_1").innerHTML=oo1+" : "
-document.getElementById("name_2").innerHTML=oo2+" : "
-function MUAHAHA(){
+  localStorage.setItem("oo1",oo1);
+  localStorage.setItem("oo2",oo2);
   window.location="moneyy.html";
-     }
-function send(){
-  var num1=document.getElementById("number_input_1").value;
-  var num2=document.getElementById("number_input_2").value;
-  number="<h4>This Guess This Equation That: "+"null"+" Has Wrote! "+"</h4>";
-  nunu="<h4> The Equation Is: "+num1+" x "+num2+" </h4>"
-  answer="<br> Type The Answer Here: <input type='text' placeholder='Type Your Answer Here'>";
-  button_check="<br><br><button class='btn btn-success' onlick='check_the_answer()'>Check Your Answer</button>";
-  var the_diver=number+nunu+answer+button_check;
-  document.getElementById("output").innerHTML=the_diver
      }
